2dfb6c05d0111036f3b095ca3b0aba80640c9cf9
Alanjid <112422437+Alanjid@users.noreply.github.com>
2023-11-14 21:11:37 -0600

    login

--- app.js
+++ app.js
@@ -3,19 +3,36 @@ const app = express();
 const dotenv = require('dotenv')
 const mysql= require("mysql");
 const cors = require ("cors");
+const bcryptjs = require('bcryptjs')
 dotenv.config({path: './env/.env'})
 const db = require('./database/db')
 app.use(cors( {origin: process.env.FRONTED_URL} ));
 app.use(express.json());
+const session = require('express-session');
+
+app.use(session({
+    secret: 'SSecret',
+    cookie: { 
+        sameSite: 'strict',
+        maxAge: null,
+        expires: null
+    },
+    saveUninitialized: false
+}));
 
 app.post("/login", (req, res) => {
     const Correo = req.body.email;
-    db.query('SELECT * FROM terapeutas WHERE correo = ?', [Correo],
-    (err, result) => {
+    const pass = req.body.password;
+    db.query('SELECT * FROM terapeutas WHERE correo = ?', [Correo], async (err, result) => {
         if(err){
             console.log(err);
         }else{
-            res.send(result);
+            if(! (await bcryptjs.compare(pass, result[0].pass))){
+                res.send({})
+            }else{
+                res.send(result);
+                
+            }
         }
     });
 });
