d87029f97083fe0025e0b54bc6a36efbfbf88edd
Alanjid <carlosalanjg123@gmail.com>
2023-11-28 11:10:57 -0600

    prueba

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ const jwt = require('jsonwebtoken');
 app.use(cors( {origin: process.env.FRONTED_URL} ));
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader('X-Frame-Options', 'DENY');
+    next();
+});
+
 app.post("/login", (req, res) => {
     const Correo = req.body.email;
     const pass = req.body.password;
@@ -21,38 +26,104 @@ app.post("/login", (req, res) => {
             if(! (await bcryptjs.compare(pass, result[0].pass))){
                 res.send({})
             }else{
-                const datosToken = {
-                    correo: result[0].correo,
-                    nombre: result[0].nombre,
-                    rol: result[0].rol,
-                };
-                const token = jwt.sign(datosToken, process.env.JWTSecret, {expiresIn: "1m"});
-                const Resultado = {
-                    token,
-                    correo: result[0].correo,
-                    nombre: result[0].nombre,
-                    app: result[0].app,
-                    apm: result[0].apm,
-                    rol: result[0].rol,
-                    idinstitucion: result[0].idinstitucion,
-                    isactive: result[0].isactive,
-                };
-                console.log(Resultado)
-                res.send(Resultado);
+                const expirar= req.body.recordarme
+                if(expirar){
+                    const datosToken = {
+                        correo: result[0].correo,
+                        nombre: result[0].nombre,
+                        app: result[0].app,
+                        apm: result[0].apm,
+                        rol: result[0].rol,
+                        idinstitucion: result[0].idinstitucion,
+                    };
+                    const token = jwt.sign(datosToken, process.env.JWTSecret);
+                    const Resultado = {
+                        token,
+                        correo: result[0].correo,
+                        nombre: result[0].nombre,
+                        app: result[0].app,
+                        apm: result[0].apm,
+                        rol: result[0].rol,
+                        idinstitucion: result[0].idinstitucion,
+                        isactive: result[0].isactive,
+                    };
+                    res.send(Resultado);
+                }else{
+                    const datosToken = {
+                        correo: result[0].correo,
+                        nombre: result[0].nombre,
+                        app: result[0].app,
+                        apm: result[0].apm,
+                        rol: result[0].rol,
+                        idinstitucion: result[0].idinstitucion,
+                    };
+                    const token = jwt.sign(datosToken, process.env.JWTSecret, {expiresIn: "8h"});
+                    const Resultado = {
+                        token,
+                        correo: result[0].correo,
+                        nombre: result[0].nombre,
+                        app: result[0].app,
+                        apm: result[0].apm,
+                        rol: result[0].rol,
+                        idinstitucion: result[0].idinstitucion,
+                        isactive: result[0].isactive,
+                    };
+                    res.send(Resultado);
+                }
             }
         }
     });
 });
 
-app.get("/user", (req, res) => {
-    db.query('SELECT * FROM terapeutas',
-    (err, result) => {
-        if(err){
-            console.log(err);
-        }else{
-            res.send(result[0]);
+app.post("/user", (req, res) => {
+    try {
+        if(req.body.token){
+            const Token = req.body.token;
+            const body = jwt.decode(Token, process.env.JWTSecret);
+            const correo = req.body.correo;
+            const nombre = req.body.nombre;
+            const app = req.body.app;
+            const apm = req.body.apm;
+            const rol = req.body.rol;
+            let idinstitucion = req.body.idinstitucion;
+            if(idinstitucion === '1'){
+                idinstitucion = 1
+            }else if(idinstitucion === '2'){
+                idinstitucion = 2
+            }
+            const arrar = {
+                correo,
+                nombre,
+                app,
+                apm,
+                rol,
+                idinstitucion,
+            }
+            try {
+                if(body.iat){
+                    delete body.iat;
+                }
+                if(body.exp){
+                    delete body.exp;
+                }
+            } catch (error) {
+                console.log('No tiene fecha de venticimiento')
+            }
+            if(JSON.stringify(arrar) === JSON.stringify(body)){
+                jwt.verify(Token, process.env.JWTSecret, function(err, decoded){
+                    if(err){
+                        res.send('false')
+                    }else{
+                        res.send('true')
+                    }
+                })
+            }else{
+                res.send('false')
+            }
         }
-    });
+    } catch (error) {
+        console.log('Algo trono xd')
+    }
 });
 
 app.listen(process.env.PORT, ()=>{
